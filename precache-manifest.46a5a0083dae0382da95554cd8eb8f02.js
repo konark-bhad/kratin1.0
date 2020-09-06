afd9fdfdcc26b186e0d5be59e9e2888ac6dc482b
konark-bhad <53810525+konark-bhad@users.noreply.github.com>
2020-09-06 17:52:00 +0530

    Updates

---
+++ precache-manifest.46a5a0083dae0382da95554cd8eb8f02.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7ddfed541377ead9ae995bbc8bfd9b4",
+    "url": "/kratin1.0/index.html"
+  },
+  {
+    "revision": "4e31090f6e34c709afd3",
+    "url": "/kratin1.0/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "8f1d599eee6cf851e9a2",
+    "url": "/kratin1.0/static/css/main.9f533c2a.chunk.css"
+  },
+  {
+    "revision": "4e31090f6e34c709afd3",
+    "url": "/kratin1.0/static/js/2.e284a509.chunk.js"
+  },
+  {
+    "revision": "7b53a0f4fcebb5e37e65b1883c203bd3",
+    "url": "/kratin1.0/static/js/2.e284a509.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f1d599eee6cf851e9a2",
+    "url": "/kratin1.0/static/js/main.f83ab09a.chunk.js"
+  },
+  {
+    "revision": "d82c895eccc87712cfe1",
+    "url": "/kratin1.0/static/js/runtime-main.22504067.js"
+  }
+]);
